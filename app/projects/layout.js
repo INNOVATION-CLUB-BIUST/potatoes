f926f9ebac13c181415baf0f88d9908b72d002bd
mutisiesabel-cloud <mutisiesabel@gmail.com>
2026-02-21 11:31:54 +0200

    feat: add demo for project (branch dev)

---
+++ app/projects/layout.js
@@ -0,0 +1,18 @@
+
+import StickyCards from "../../components/StickyCards/StickyCards";
+
+export default function Home() {
+    return (
+        <>
+         <section className="intro">
+            <h1>The Foundations</h1>
+         </section>
+
+         <StickyCards />
+
+         <section className="outro">
+            <h1>Ends in Form</h1>
+         </section>
+        </>
+    );
+}
